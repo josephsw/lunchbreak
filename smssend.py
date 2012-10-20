39cb2c606211a63fe14a434765e4fc81cdc3328e
Joseph Wang <joseph.wang@utexas.edu>
2012-10-20 10:34:10 -0500

    Added additional documentation

--- smssend.py
+++ smssend.py
@@ -34,13 +34,19 @@ class SmsSend(webapp.RequestHandler):
         path = os.path.join(os.path.dirname(__file__), 'index.html')
         self.response.out.write(template.render(path, template_values))
 
+
+    #THE GOOD STUFF
+    #make sure the twilio library (folder) is there
+    #do the import above for twilio.rest
+    #sms_number takes a 10-digit number as an arg1, and a message string as arg2
+    # This function will send the number specified the given message.
     def sms_number(self, number, msg):
-        SIZE = 155
         account = "AC2bca3b527c9d4be4956b0cb8374f981a"
         token = "29ca474294d5555b1b3dc5ed046a40f8"
+        SIZE = 155
         client = TwilioRestClient(account, token)
         num_messages = len(msg) / SIZE + 1
-        #number = number.strip().replace(" ", "").replace("-","")
+        number = number.strip().replace(" ", "").replace("-","")
         if len(number) == 10:
             if num_messages == 1:
                 client.sms.messages.create(to="+1"+number, from_="+15123944123", body=msg)
